3494d049bdde2f5a769fa92c11a272a268447d96
Enrique Vierne <viernee@gmail.com>
2023-04-29 16:21:34 -0300

    feat: add Input component.

--- src/components/Form/LoginForm/index.tsx
+++ src/components/Form/LoginForm/index.tsx
@@ -1,10 +1,17 @@
+import { SubmitHandler, useForm } from "react-hook-form";
 import { useState } from "react"
 import { zodResolver } from "@hookform/resolvers/zod";
+import { TLoginFormValues, loginFormSchema } from "./loginFormSchema";
 
 export const LoginForm = () => {
     
     const [loading, setLoading] = useState();
     const {register, handleSubmit, formState: {errors},
-} = useForm({zodResolver(loginFormSchema)})
-    return()
+} = useForm<TLoginFormValues>({resolver: zodResolver(loginFormSchema)})
+    return(
+        <form>
+            <input type="text" />
+        </form>
+    )
 }
+
