4ded60dd1b50b70e9d06587b30b91ae31df3ca4e
Enrique Vierne <viernee@gmail.com>
2023-04-29 16:44:08 -0300

    feat: userLogin and loginForm

---
+++ src/providers/userContext.tsx
@@ -0,0 +1,55 @@
+import { createContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { TLoginFormValues } from "../components/Form/LoginForm/loginFormSchema";
+import { api } from "../services/api";
+
+interface IUserContext {
+  userLogin: (
+    formData: TLoginFormValues,
+    setLoading: React.Dispatch<React.SetStateAction<boolean>>
+  ) => Promise<void>;
+}
+
+interface IUser {
+  name: string;
+  email: string;
+  id: string;
+}
+
+interface IUserProviderProps {
+  children: React.ReactNode;
+}
+
+interface IUserLoginResponse {
+  accessToken: string;
+  user: IUser;
+}
+
+export const UserContext = createContext({} as IUserContext);
+export const UserProvider = ({ children }: IUserProviderProps) => {
+  const navigate = userNavigate();
+
+  const userLogin = async (
+    formData: TLoginFormValues,
+    setLoading: React.Dispatch<React.SetStateAction<boolean>>
+  ) => {
+    try {
+      setLoading(true);
+      const { data } = await api.post<IUserLoginResponse>("/login", formData);
+      localStorage.setItem("@TOKEN_KenzieBurguer", data.accessToken);
+      localStorage.setItem("@USERID_KenzieBurguer", data.user.id);
+      /* toast.success("Login bem sucedido");
+      navigate("/dashboard"); */
+    } catch (error: any) {
+      console.log(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <UserContext.Provider value={{  userLogin }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
